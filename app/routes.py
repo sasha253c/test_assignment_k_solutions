ada896296f967a6eec0013b1b4b406aa0754b85d
sasha253c <kochynsasha253@gmail.com>
2019-01-08 21:51:22 +0200

    mini app

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from app.forms import PaymentForm
 from app.strategies import choose_strategy
 
 from flask import request, flash, redirect, url_for, render_template
-from app.logger import LOGGER
+# from app.logger import LOGGER
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -14,12 +14,13 @@ def index():
     form = PaymentForm()
     if form.validate_on_submit() and request.method == 'POST':
         flash(f'Form data: {form.data}')
-        strategy = choose_strategy(currency=form.currency.data)
+        # strategy = choose_strategy(currency=form.currency.data)
         # Todo: add log massage with time, currency, amount and discribe
-        LOGGER.info(f"Payment: {request.form.to_dict()}")
+        # LOGGER.info(f"Payment: {request.form.to_dict()}")
         print('FORM: ', request.form.to_dict())
         print('*'*80)
-        res = strategy.execute(params=request.form.to_dict())
-        flash(f'RES: {res}')
+        # res = strategy.execute(params=request.form.to_dict())
+        # flash(f'RES: {res}')
+        res = "hello index"
         return res
     return render_template('base.html', form=form)
